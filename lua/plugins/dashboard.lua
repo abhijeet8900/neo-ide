27ad4cd02980989f9a3db63024625325714adee4
abhijeet-tilekar@techverito <abhijeet.tilekar@techverito.com>
2024-12-31 12:37:04 +0530

    - Update dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,4 +1,28 @@
 return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      config = {
+        -- header:  -- type is table def
+        week_header = {
+          enable = true, --boolean use a week header
+          -- concat  --concat string after time string line
+          -- append  --table append after time string line
+        },
+        -- disable_move  -- boolean default is false disable move key
+      },
+      hide = {
+        statusline = false, -- hide statusline default is true
+        tabline = true, -- hide the tabline
+        winbar = false, -- hide winbar
+      },
+    }
+  end,
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+}
+--[=====[ 
+return {
   'goolord/alpha-nvim',
   dependencies = {
     'nvim-tree/nvim-web-devicons',
@@ -22,3 +46,5 @@ return {
     alpha.setup(dashboard.opts)
   end,
 }
+--]=====]
+--
